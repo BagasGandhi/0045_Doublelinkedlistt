9cfad025d24fd5098ed0e8f64ee5e1b04803a042
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-04-30 21:32:38 +0700

    Implement deletion confirmation message in deleteNode method of DoubleLinkedList

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -110,5 +110,8 @@ public:
 
             current->prev->next = current->next;
         }
+
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 };
